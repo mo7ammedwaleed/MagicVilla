4a1668a226983206e482d959ecdf96ed74a16916
Mohammed Waleed <mwaleedx1@gmail.com>
2025-04-12 00:24:23 +0200

    Seperate DTO for Create and Update

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -49,41 +49,41 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaCreateDTO createDTO)
         {
             //if(!ModelState.IsValid)
             //{
             //    return BadRequest(ModelState);
             //}
-            if (_db.Villas.FirstOrDefault(e => e.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            if (_db.Villas.FirstOrDefault(e => e.Name.ToLower() == createDTO.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("CustomError", "Villa already exists!");
                 return BadRequest(ModelState);
             }
-            if (villaDTO == null)
+            if (createDTO == null)
             {
-                return BadRequest(villaDTO);
+                return BadRequest(createDTO);
             }
-            if (villaDTO.Id > 0)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
+            //if (createDTO.Id > 0)
+            //{
+            //    return StatusCode(StatusCodes.Status500InternalServerError);
+            //}
 
             Villa model = new()
             {
-                Id = villaDTO.Id,
-                Name = villaDTO.Name,
-                Details = villaDTO.Details,
-                Sqft = villaDTO.Sqft,
-                Occupancy = villaDTO.Occupancy,
-                Rate = villaDTO.Rate,
-                ImageUrl = villaDTO.ImageUrl,
-                Amenity = villaDTO.Amenity
+                //Id = createDTO.Id,
+                Name = createDTO.Name,
+                Details = createDTO.Details,
+                Sqft = createDTO.Sqft,
+                Occupancy = createDTO.Occupancy,
+                Rate = createDTO.Rate,
+                ImageUrl = createDTO.ImageUrl,
+                Amenity = createDTO.Amenity
             };
             _db.Villas.Add(model);
             _db.SaveChanges();
 
-            return CreatedAtRoute("GetVilla", new {id = villaDTO.Id},villaDTO);
+            return CreatedAtRoute("GetVilla", new {id = model.Id}, model);
         }
 
         [HttpDelete("{id}",Name = "DeleteVilla")]
@@ -109,9 +109,9 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpPut("{id}", Name = "UpdateVilla")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+        public IActionResult UpdateVilla(int id, [FromBody]VillaUpdateDTO updateDTO)
         {
-            if (villaDTO == null || id != villaDTO.Id)
+            if (updateDTO == null || id != updateDTO.Id)
             {
                 return BadRequest();
             }
@@ -122,14 +122,14 @@ namespace MagicVilla_VillaAPI.Controllers
 
             Villa model = new()
             {
-                Id = villaDTO.Id,
-                Name = villaDTO.Name,
-                Details = villaDTO.Details,
-                Sqft = villaDTO.Sqft,
-                Occupancy = villaDTO.Occupancy,
-                Rate = villaDTO.Rate,
-                ImageUrl = villaDTO.ImageUrl,
-                Amenity = villaDTO.Amenity
+                Id = updateDTO.Id,
+                Name = updateDTO.Name,
+                Details = updateDTO.Details,
+                Sqft = updateDTO.Sqft,
+                Occupancy = updateDTO.Occupancy,
+                Rate = updateDTO.Rate,
+                ImageUrl = updateDTO.ImageUrl,
+                Amenity = updateDTO.Amenity
             };
             _db.Villas.Update(model);
             _db.SaveChanges();
@@ -140,15 +140,15 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpPatch("{id}", Name = "UpdatePartialVilla")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public IActionResult UpdatePartialVilla(int id,JsonPatchDocument<VillaDTO> patchDTO)
+        public IActionResult UpdatePartialVilla(int id,JsonPatchDocument<VillaUpdateDTO> patchUpdateDTO)
         {
-            if(patchDTO == null || id == 0)
+            if(patchUpdateDTO == null || id == 0)
             {
                 return BadRequest();
             }
             var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
 
-            VillaDTO villaDTO = new()
+            VillaUpdateDTO villaUpdateDTO = new()
             {
                 Id = villa.Id,
                 Name = villa.Name,
@@ -165,18 +165,18 @@ namespace MagicVilla_VillaAPI.Controllers
                 return BadRequest();
             }
 
-            patchDTO.ApplyTo(villaDTO, ModelState);
+            patchUpdateDTO.ApplyTo(villaUpdateDTO, ModelState);
 
             Villa model = new()
             {
-                Id = villaDTO.Id,
-                Name = villaDTO.Name,
-                Details = villaDTO.Details,
-                Sqft = villaDTO.Sqft,
-                Occupancy = villaDTO.Occupancy,
-                Rate = villaDTO.Rate,
-                ImageUrl = villaDTO.ImageUrl,
-                Amenity = villaDTO.Amenity
+                Id = villaUpdateDTO.Id,
+                Name = villaUpdateDTO.Name,
+                Details = villaUpdateDTO.Details,
+                Sqft = villaUpdateDTO.Sqft,
+                Occupancy = villaUpdateDTO.Occupancy,
+                Rate = villaUpdateDTO.Rate,
+                ImageUrl = villaUpdateDTO.ImageUrl,
+                Amenity = villaUpdateDTO.Amenity
             };
             _db.Villas.Update(model);
             _db.SaveChanges();
