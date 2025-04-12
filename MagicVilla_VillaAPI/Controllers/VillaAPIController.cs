3e398a81e98a2b620fe979992d768484b448fa87
Mohammed Waleed <mwaleedx1@gmail.com>
2025-04-12 03:17:03 +0200

    API Response Class

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,12 +1,9 @@
-﻿using System.Threading.Tasks;
-using AutoMapper;
-using MagicVilla_VillaAPI.Data;
+﻿using AutoMapper;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using MagicVilla_VillaAPI.Repository.IRepository;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
