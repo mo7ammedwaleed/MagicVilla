d02f19183dff211e8a8d8c00c3eb904e069e5f1f
Mohammed Waleed <mwaleedx1@gmail.com>
2025-05-07 21:04:02 +0300

    Auth Controller Action Methods

--- MagicVilla_Web/Services/IServices/IAuthService.cs
+++ MagicVilla_Web/Services/IServices/IAuthService.cs
@@ -5,6 +5,6 @@ namespace MagicVilla_Web.Services.IServices
     public interface IAuthService
     {
         Task<T> LoginAsync<T>(LoginRequestDTO loginRequestDTO);
-        Task<T> RegisterAsync<T>(UserDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegisterationRequestDTO objToCreate);
     }
 }
