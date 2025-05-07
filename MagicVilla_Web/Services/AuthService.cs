27a528067b2d4bf38d231047060119ab5936895d
Mohammed Waleed <mwaleedx1@gmail.com>
2025-05-07 21:09:58 +0300

    Login And Register Views

--- MagicVilla_Web/Services/AuthService.cs
+++ MagicVilla_Web/Services/AuthService.cs
@@ -24,7 +24,7 @@ namespace MagicVilla_Web.Services
             });
         }
 
-        public Task<T> RegisterAsync<T>(UserDTO obj)
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
         {
             return SendAsync<T>(new APIRequest()
             {
